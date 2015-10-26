871df6856bacbc212ced2ad7862f9337648e948b
Adam Hale (i7685032) <i7685032@mac27866.local>
2015-10-26 12:09:09 +0000

    Initial Commit

---
+++ workshop5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  workshop5
+//
+//  Created by Adam Hale (i7685032) on 26/10/2015.
+//  Copyright © 2015 Adam Hale (i7685032). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
